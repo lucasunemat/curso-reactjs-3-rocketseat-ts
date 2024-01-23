4faadeac5bbebdd9a4528ea11d405c3fd026373b
Lucas Nunes Batista <lucas.batista@unemat.br>
2024-01-23 15:34:57 -0400

    criando searchform

---
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -0,0 +1,11 @@
+// criando o SearchForm dentro do Transactions porque ele é usado apenas na página de Transactions
+
+import { SearchFormContainer } from "./styles";
+
+export function SearchForm() {
+    return (
+        <SearchFormContainer>
+
+        </SearchFormContainer>
+    )
+}
