00d0c8ef18278b8e41209f398f63061a0a457234
Lucas Nunes Batista <lucas.batista@unemat.br>
2024-01-24 13:47:52 -0400

    primeira requisição já sendo feita

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,24 +1,35 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionContainer, TransactionsTable } from "./styles";
 
+interface Transaction {
+    id: number;
+    description: string;
+    type: 'income' | 'outcome';
+    price: number;
+    category: string;
+    createdAt: string;
+}
+
 export function Transactions() {
+
+    const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+
     //fazendo useEffect vigiar o carregamento da página para fazer requisição ao iniciar a página
+    // fetch('http://localhost:3000/Transactions')
+    // .then (response => response.json())
+    // .then (data => console.log(data))
+    async function loadTransactions() {
+        const response = await fetch('http://localhost:3000/Transactions');
+        const data = await response.json();
+
+        setTransactions(data);
+    }
     useEffect(() => {
-        // fetch('http://localhost:3000/Transactions')
-        // .then (response => response.json())
-        // .then (data => console.log(data))
-
-        async function loadTransactions() {
-            const response = await fetch('http://localhost:3000/Transactions')
-            const data = await response.json()
-
-            console.log(data)
-        }
-
-        loadTransactions()
+        loadTransactions();
     }, []);
 
     return (
@@ -27,21 +38,19 @@ export function Transactions() {
             <Summary />
 
             <TransactionContainer>
-                <SearchForm/>
+                <SearchForm />
                 <TransactionsTable>
                     <tbody>
-                        <tr>
-                            <td width="50%">Desenvolvimento de website</td>
-                            <td><PriceHighlight variant="income">R$ 12.000,00</PriceHighlight></td>
-                            <td>Venda</td>
-                            <td>02/04/2024</td>
-                        </tr>
-                        <tr>
-                            <td width="50%">Alimentação</td>
-                            <td><PriceHighlight variant="outcome"> -R$ 1.090,00</PriceHighlight></td>
-                            <td>Venda</td>
-                            <td>02/04/2024</td>
-                        </tr>
+                        {transactions.map(transaction => {
+                            return (
+                                <tr key={transaction.id}>
+                                    <td width="50%">{transaction.description}</td>
+                                    <td><PriceHighlight variant={transaction.type}>{transaction.price}</PriceHighlight></td>
+                                    <td>{transaction.category}</td>
+                                    <td>{transaction.createdAt}</td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </TransactionsTable>
             </TransactionContainer>
